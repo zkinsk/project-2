08bd9fb527fe9fa6933fd1867482df5f6ccde2a3
Andrew Dawson <dawso.andrew@gmail.com>
2019-03-04 23:51:33 -0500

    Adds a day page with a park listing and map.

---
+++ public/js/day.js
@@ -0,0 +1,53 @@
+"use strict";
+
+let infoWindow;
+let map;
+let service;
+
+function createMarker(place) {
+  const marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location,
+  });
+
+  google.maps.event.addListener(marker, "click", function() {
+    infoWindow.setContent(place.name);
+    infoWindow.open(map, this);
+  });
+}
+
+function addPlace(name) {
+  const request = {
+    query: name,
+    fields: ["name", "geometry"],
+  };
+
+  service.findPlaceFromQuery(request, (results, status) => {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      createMarker(results[0]);
+    }
+  });
+}
+
+function initMap() {
+  map = new google.maps.Map($("#map")[0], {
+    center: {lat: 37.5407, lng: -77.4360},
+    zoom: 11,
+  });
+
+  infoWindow = new google.maps.InfoWindow();
+
+  service = new google.maps.places.PlacesService(map);
+
+  const places = [
+    "Barker Field",
+    "Rockwood Park",
+    "Church Hill Dog Park",
+    "Northside Dog Park",
+    "Phideaux Dog Park",
+  ];
+
+  for(const name of places) {
+    addPlace(name);
+  }
+}
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,32 +1,37 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
-  
-  app.get("/user/profile", (request, response) => {
-      response.render("Profile", {
-        title: 'User Profile',
-      });
-  });
-  
-  app.get("/user/new", (request, response) => {
-      response.render("setup", {
-        title: 'New User',
-      });
-  });
-
   app.get("/calendar", (request, response) => {
     response.render("calendar", {
       title: "Calendar",
     });
   });
 
+  app.get("/day", (request, response) => {
+    response.render("day", {
+      title: "Day",
+    });
+  });
+
+  app.get("/user/profile", (request, response) => {
+      response.render("Profile", {
+        title: "User Profile",
+      });
+  });
+  
+  app.get("/user/new", (request, response) => {
+      response.render("setup", {
+        title: "New User",
+      });
+  });
+
+  // Load index page
   app.get("/", (request, response) => {
     db.Dog
       .findAll({})
       .then((results) => {
         response.render("index", {
-          title: 'Dogs Day Out',
+          title: "Dogs Day Out",
           msg: "Welcome!",
           dogs: results,
         });
