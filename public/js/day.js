303cf877fcbe5bbc82a96de8be96203f45b0add2
Andrew Dawson <dawso.andrew@gmail.com>
2019-03-06 20:25:00 -0500

    The day page is now populated with actual parks and times for events.

--- public/js/day.js
+++ public/js/day.js
@@ -29,6 +29,62 @@ function addPlace(name) {
   });
 }
 
+function addEventsToList(events) {
+  $("#park-list").empty();
+
+  let parkName = null;
+
+  let timeList = null;
+  let parkItem = null;
+  
+  for (const event of events) {
+    if (event.Park.name != parkName) {
+      parkName = event.Park.name;
+
+      parkItem = $("<li>");
+      parkItem.text(parkName);
+      $("#park-list").append(parkItem);
+
+      timeList = $("<ul>");
+      parkItem.append(timeList);
+    }
+
+    const timeItem = $("<li>");
+
+    const timeLink = $("<a>");
+    timeLink.attr("href", "/chat/" + event.id);
+    timeLink.text(event.time);
+
+    timeItem.append(timeLink);
+
+    timeList.append(timeItem);
+  }
+}
+
+/** Define an order for the time of day names, since alphabetical doesn't make sense. */
+function getTimeSortOrder(time) {
+  switch (time) {
+    case "Morning":   return 0;
+    case "Afternoon": return 1;
+    case "Evening":   return 2;
+    default:          return 0;
+  }
+}
+
+/** Sort by park name, then by time of day. */
+function sortEvents(events) {
+  events.sort((a, b) => {
+    if (a.Park.name < b.Park.name) {
+      return -1;
+    }
+    if (a.Park.name > b.Park.name) {
+      return 1;
+    }
+    return getTimeSortOrder(a.time) - getTimeSortOrder(b.time);
+  });
+}
+
+// This is the entry point callback for when a connection to the google maps API is established.
 function initMap() {
   map = new google.maps.Map($("#map")[0], {
     center: {lat: 37.5407, lng: -77.4360},
@@ -52,7 +108,8 @@ function initMap() {
   }
 
   $.get("/api/event?date=" + date)
-    .then((response) => {
-      console.log(response);
+    .then((events) => {
+      sortEvents(events);
+      addEventsToList(events);
     });
 }
