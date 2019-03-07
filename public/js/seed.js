a96a1e981d8ac2b5b504ba25d953fc5fda3512f9
Zkinsk <zoolunk@gmail.com>
2019-03-06 21:41:46 -0500

    added park seeds so I can revise google map

---
+++ public/js/seed.js
@@ -0,0 +1,27 @@
+var park = [
+  {
+    name: "Barker Field",
+    lat: 37.5391116,
+    lon: -77.4842119
+  },
+  {
+    name: "Churchill Dog Park",
+    lat: 37.5239282,
+    lon: -77.4126637
+  },
+  {
+    name: "Phideaux Dog Park",
+    lat: 37.5198621,
+    lon: -77.483193
+  },
+  {
+    name: "Northside Dog Park",
+    lat: 37.5977001,
+    lon: -77.4436564
+  },
+  {
+    name: "Rockwood Park",
+    lat: 37.4469508,
+    lon: -77.5840857
+  }
+];
