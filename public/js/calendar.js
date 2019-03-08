9be480b8f74e943e22f9563da51821e4bf163ea8
alexaoswald <alexaoswald1@gmail.com>
2019-03-07 21:23:56 -0500

    basic calendar

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,9 +1,40 @@
-"use strict";
 
-// TEST CODE! Remove this!
-$(() => {
-  $.get("/api/event/date?month=2019-03")
-    .then((response) => {
-      console.log(response);
+
+$(document).ready(function(){
+  var now = moment().format();
+  console.log(now);
+  // var calendar = $('#calendar').fullCalendar('getCalendar');
+  $(function() {
+
+    $('#calendar').fullCalendar({
+      selectable: true,
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay'
+      },
+      dayClick: function(date) {
+        alert('clicked ' + date.format());
+      },
+      
     });
+  
+  });
 });
+
+// $('#calendar').fullCalendar({
+//   dayClick: function(date, jsEvent, view) {
+
+//     alert('Clicked on: ' + date.format());
+
+//     alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+
+//     alert('Current view: ' + view.name);
+
+//     // change the day's background color just for fun
+//     $(this).css('background-color', 'red');
+
+//   }
+// });
+
+  
